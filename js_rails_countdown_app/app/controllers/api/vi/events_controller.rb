5afe3f7e815aebb4b7a9515a94f509eaafd4aaec
Heather <heather.miller114@gmail.com>
2020-03-19 15:03:50 -0400

    adds index and update actions for event

--- js_rails_countdown_app/app/controllers/api/vi/events_controller.rb
+++ js_rails_countdown_app/app/controllers/api/vi/events_controller.rb
@@ -1,2 +1,31 @@
 class Api::Vi::EventsController < ApplicationController
+
+    before_action :find_event, only: [:update]
+
+    def index
+        @events = Event.all
+        render json: @events
+    end
+
+    def update
+        @event.update(event_params)
+        if @event.save
+            render json: @event, status: :accepted
+        else
+            render json: { errors: @event.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+
+
+    private
+
+    def event_params
+        params.permit(:id, :date, :name)
+    end
+
+    def find_event
+        @event = Event.find(params[:id])
+    end
+
 end
