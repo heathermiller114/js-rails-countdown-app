9c5551da55a09be8052b73fa6618fceb16b00ef9
Heather <heather.miller114@gmail.com>
2020-04-22 14:29:23 -0400

    adds create route to events

--- js_rails_countdown_app/config/routes.rb
+++ js_rails_countdown_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :events, only: [:index, :update, :destroy]
+      resources :events, only: [:index, :create, :update, :destroy]
     end
   end
 end
