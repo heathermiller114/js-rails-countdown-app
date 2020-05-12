ebf038132dff5a41b9138879ef561778ed9f5b4b
Heather <heather.miller114@gmail.com>
2020-05-12 19:44:58 -0400

    adds user routes to config/routes.rb

--- js_rails_countdown_app/config/routes.rb
+++ js_rails_countdown_app/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :events, only: [:index, :create, :update, :destroy]
+      resource :users, only: [:create]
+      post "/login", to: "users#login"
+      get "/auto_login", to: "users#auto_login"
     end
   end
 end
